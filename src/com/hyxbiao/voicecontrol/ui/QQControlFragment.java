8e1e8fe4847982864082bd7db4c3cae27295a79b
hyxbiao <hyxbiao@gmail.com>
2013-07-04 18:59:19 +0800

    update

--- src/com/hyxbiao/voicecontrol/ui/QQControlFragment.java
+++ src/com/hyxbiao/voicecontrol/ui/QQControlFragment.java
@@ -1,14 +1,16 @@
 package com.hyxbiao.voicecontrol.ui;
 
 import com.hyxbiao.voicecontrol.client.R;
+import com.hyxbiao.voicecontrol.command.VoiceCommandManager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 
-public class QQControlFragment extends Fragment {
+public class QQControlFragment extends Fragment implements OnClickListener {
 	
 	
 	public QQControlFragment() { 
@@ -18,12 +20,50 @@ public class QQControlFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		getActivity().setTitle(getResources().getString(R.string.qq_control_title));
-		return inflater.inflate(R.layout.qq_content, null);
+		View v = inflater.inflate(R.layout.qq_content, null);
+		v.findViewById(R.id.btn_qq_open).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_close).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_video_baobao).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_video_test).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_video_screen_max).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_video_screen_min).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_video_screen_max).setOnClickListener(this);
+		v.findViewById(R.id.btn_qq_video_close).setOnClickListener(this);
+		return v;
 	}
 	
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public void onClick(View v) {
+		VoiceCommandManager commandManager = ((MyApp) getActivity().getApplicationContext()).getCommandManager();
+		int id = v.getId();
+        switch (id) {
+	        case R.id.btn_qq_open:
+	        	commandManager.execute(getString(R.string.qq_open));
+	        	break;
+	        case R.id.btn_qq_close:
+	        	commandManager.execute(getString(R.string.system_home));
+	        	break;
+	        case R.id.btn_qq_video_baobao:
+	        	commandManager.execute(getString(R.string.qq_video_baobao));
+	        	break;
+	        case R.id.btn_qq_video_test:
+	        	commandManager.execute(getString(R.string.qq_video_test));
+	        	break;
+	        case R.id.btn_qq_video_screen_max:
+	        	commandManager.execute(getString(R.string.qq_video_screen_max));
+	        	break;
+	        case R.id.btn_qq_video_screen_min:
+	        	commandManager.execute(getString(R.string.qq_video_screen_min));
+	        	break;
+	        case R.id.btn_qq_video_close:
+	        	commandManager.execute(getString(R.string.qq_video_close));
+	        	break;
+        };
 	}
 	
 }
--- src/com/hyxbiao/voicecontrol/ui/SettingFragment.java
+++ src/com/hyxbiao/voicecontrol/ui/SettingFragment.java
@@ -2,11 +2,16 @@ package com.hyxbiao.voicecontrol.ui;
 
 import com.hyxbiao.voicecontrol.client.R;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.format.Formatter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SettingFragment extends Fragment {
 	
@@ -18,9 +23,26 @@ public class SettingFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		getActivity().setTitle(getResources().getString(R.string.setting_title));
-		return inflater.inflate(R.layout.setting, null);
+		View v = inflater.inflate(R.layout.setting, null);
+		
+		MyApp myApp = (MyApp) getActivity().getApplicationContext();
+		WifiManager wifiMgr = (WifiManager) myApp.getSystemService(Context.WIFI_SERVICE);
+		WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
+		int ip = wifiInfo.getIpAddress();
+		String ipAddress = Formatter.formatIpAddress(ip);
+		TextView ipTextView = (TextView) v.findViewById(R.id.textView1);
+		ipTextView.setText(ipAddress);
+		
+		return v;
 	}
-	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+	}
+
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
